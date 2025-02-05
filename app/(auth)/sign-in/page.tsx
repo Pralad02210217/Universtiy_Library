a7088d0a36f25b2e99cd09845f86224f5c59f158
pralad02210217 <praladmishra3@gmail.com>
2025-02-05 23:25:58 +0600

    done with basic Form UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+import AuthForm from '@/components/AuthForm'
+import { signInSchema } from '@/lib/validations'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Sign In Form</div>
+    <AuthForm type='SIGN_IN' schema ={signInSchema} defaultValues ={{
+        email: "",
+        password: "",
+    }}
+    onSubmit ={() => {}}
+    />
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+import AuthForm from '@/components/AuthForm'
+import { signUpSchema } from '@/lib/validations'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Sign Up Form</div>
+    <AuthForm type='SIGN_UP' schema ={signUpSchema} defaultValues ={{
+        email: "",
+        password: "",
+        fullName:"",
+        universityId: 0,
+        universityCard: ""
+    }}
+    onSubmit ={() => {}}
+    />
   )
 }
 
